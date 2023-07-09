4dd5308eedbd424184d0e5af384f24b9e828f715
iman <iman.shahabi@takwinco.com>
2023-07-09 22:58:46 +0300

    add NestedScrollView

--- lib/widgets/tab_bar_Ex.dart
+++ lib/widgets/tab_bar_Ex.dart
@@ -55,124 +55,99 @@ class _HomeViewSmallState extends State<HomeViewSmall> {
   int selectedLocaleIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 7,
-      child: Stack(children: [
-        Image.asset(
-          "assets/images/fawz_bkCopy.jpg",
-          width: double.infinity,
-          height: double.infinity,
-          fit: BoxFit.fill,
-        ),
-        Scaffold(
-          resizeToAvoidBottomInset: true,
-          backgroundColor: Colors.transparent,
-          appBar: AppBar(
-            actions: popmenubutton,
-            backgroundColor: Color.fromARGB(29, 241, 241, 22),
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(330.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(0.0),
-                    child: SizedBox(
-                      width: 100,
-                      height: 200,
-                      child: Container(
-                          padding: EdgeInsets.all(100),
-                          margin: EdgeInsets.only(bottom: 0),
-                          child: null),
-                    ),
+    return Scaffold(
+      body: DefaultTabController(
+        length: 7,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                actions: popmenubutton,
+                pinned: false,
+                expandedHeight: 330.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  //   title: Text('Goa', textScaleFactor: 1),
+                  background: Image.asset(
+                    'assets/images/fawz_bk.jpg',
+                    fit: BoxFit.fill,
                   ),
-                  Material(
-                    child: TabBar(
-                      isScrollable: true,
-                      indicator: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(10), // Creates border
-                          color: Color.fromARGB(111, 245, 228, 207)),
-                      labelPadding:
-                          EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-                      splashBorderRadius: BorderRadius.circular(5),
-                      labelColor: Colors.white,
-                      tabs: DUMMY[selectedLocaleIndex]
-                          .map((catgory) => TabResturant(
-                                catgory.imageicon,
-                                catgory.types,
-                              ))
-                          .toList(),
-                    ),
-                  ),
-                  Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.centerRight,
-                          end: Alignment.center,
-                          colors: const [
-                            Colors.black,
-                            Colors.black,
-                          ],
-                        ),
-                      ),
-                      child: Text('')),
-                ],
+                  stretchModes: [StretchMode.zoomBackground],
+                ),
+                //collapsedHeight: 100,
               ),
-            ),
-          ),
-          body: Padding(
-            padding: EdgeInsets.all((widget.widthLayout / 50).toDouble()),
-            child: TabBarView(
-              children: datalacale[selectedLocaleIndex]
-                  .map((category) => GridView(
-                        gridDelegate:
-                            // ignore: prefer_const_constructors
-                            SliverGridDelegateWithMaxCrossAxisExtent(
-                                maxCrossAxisExtent:
-                                    (widget.widthLayout) * widget.ratio,
-                                childAspectRatio: 3 / 4,
-                                crossAxisSpacing: 5,
-                                mainAxisSpacing: 1),
-                        children: (category['restaurants']
-                                as List<Map<String, dynamic>>)
-                            .map(
-                              (restaurant) => Column(
-                                children: [
-                                  InkWell(
-                                    onTap: () {
-                                      selectMeal(context);
-                                      print(widget.ratio);
-                                    },
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          restaurant['namemain'],
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        CardCategory(
-                                            restaurant['name'],
-                                            restaurant['imageUrl'],
-                                            restaurant['price'],
-                                            widget.widthLayout,
-                                            widget.ratio),
-                                      ],
-                                    ),
+              SliverPersistentHeader(
+                delegate: MySliverPersistentHeaderDelegate(
+                  TabBar(
+                    isScrollable: true,
+                    // indicatorSize: ,
+                    padding: EdgeInsets.all(10),
+                    indicator: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(10), // Creates border
+                        color: Color.fromARGB(111, 245, 228, 207)),
+                    labelPadding:
+                        EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                    splashBorderRadius: BorderRadius.circular(5),
+                    labelColor: Colors.white,
+                    tabs: DUMMY[selectedLocaleIndex]
+                        .map((catgory) => TabResturant(
+                              catgory.imageicon,
+                              catgory.types,
+                            ))
+                        .toList(),
+                  ),
+                ),
+                pinned: false,
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: datalacale[selectedLocaleIndex]
+                .map((category) => GridView(
+                      gridDelegate:
+                          // ignore: prefer_const_constructors
+                          SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent:
+                                  (widget.widthLayout) * widget.ratio,
+                              childAspectRatio: 3 / 4,
+                              crossAxisSpacing: 5,
+                              mainAxisSpacing: 1),
+                      children: (category['restaurants']
+                              as List<Map<String, dynamic>>)
+                          .map(
+                            (restaurant) => Column(
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    selectMeal(context);
+                                    print(widget.ratio);
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        restaurant['namemain'],
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      CardCategory(
+                                          restaurant['name'],
+                                          restaurant['imageUrl'],
+                                          restaurant['price'],
+                                          widget.widthLayout,
+                                          widget.ratio),
+                                    ],
                                   ),
-                                ],
-                              ),
-                            )
-                            .toList(),
-                      ))
-                  .toList(),
-            ),
+                                ),
+                              ],
+                            ),
+                          )
+                          .toList(),
+                    ))
+                .toList(),
           ),
         ),
-      ]),
+      ),
     );
   }
 
@@ -270,5 +245,29 @@ class _HomeViewSmallState extends State<HomeViewSmall> {
         ),
       ),
     ];
+  }
+}
+
+class MySliverPersistentHeaderDelegate extends SliverPersistentHeaderDelegate {
+  MySliverPersistentHeaderDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => 100;
+  @override
+  double get maxExtent => 132;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(MySliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
